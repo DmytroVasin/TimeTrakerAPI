962084d2ad70256ff00dbd2a637037dde732172c
Dmytro Vasin <dmytro.vasin@gmail.com>
2016-08-19 10:16:08 +0300

    Forsed migrations

--- db/migrate/20160815125440_add_tasks.rb
+++ db/migrate/20160815125440_add_tasks.rb
@@ -1,7 +1,7 @@
 class AddTasks < ActiveRecord::Migration[5.0]
   def change
     create_table :tasks do |t|
-      t.string :time
+      t.integer :time, default: 0, null: false
       t.text :comment
 
       t.integer :story_id
--- db/migrate/20160819062645_change_type_of_time_column.rb
+++
@@ -1,9 +0,0 @@
-class ChangeTypeOfTimeColumn < ActiveRecord::Migration[5.0]
-  def up
-    change_column :tasks, :time, :integer, default: 0, null: false
-  end
-
-  def down
-    change_column :tasks, :time, :string
-  end
-end
