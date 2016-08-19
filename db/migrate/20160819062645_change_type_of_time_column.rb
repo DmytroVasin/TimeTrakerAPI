03679915945f379068b762073381449d19cef9d7
Dmytro Vasin <dmytro.vasin@gmail.com>
2016-08-19 10:13:52 +0300

    Revertable migration

--- db/migrate/20160819062645_change_type_of_time_column.rb
+++ db/migrate/20160819062645_change_type_of_time_column.rb
@@ -1,5 +1,9 @@
 class ChangeTypeOfTimeColumn < ActiveRecord::Migration[5.0]
-  def change
+  def up
     change_column :tasks, :time, :integer, default: 0, null: false
+  end
+
+  def down
+    change_column :tasks, :time, :string
   end
 end
